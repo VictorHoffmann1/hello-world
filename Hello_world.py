3874fd1159a1969bd59a481cd18e06d90e1fc467
Lucas Kuhn <lucaskuhn67@gmail.com>
2022-06-10 15:27:54 +0200

    LenNouveaufichierpyton

--- Hello_world.py
+++ Hello_world.py
@@ -7,6 +7,6 @@ print("Hello world")
 
 # We introduce some interaction with the user
 
-name = input("What's your name?")
+name = input("What's your name?My name is Lucas")
 print("\nHello " + name)
 
